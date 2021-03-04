558c4297466768baaa6f3c3e7adbd07b4761e307
kalyani <kalyani_sodem@yahoo.com>
2021-03-03 22:21:49 -0600

    Added Remove functionality

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,20 +1,35 @@
 import React from 'react'
 import "./Checkout.css";
 import Subtotal from "./Subtotal.js";
+import { useStateValue } from "./StateProvider.js";
+import CheckoutProduct from "./CheckoutProduct.js";
 
 
 function Checkout() {
+  const [{basket}, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
         <img className="checkout__ad"
          src="https://images-na.ssl-images-amazon.com/images/G/01/AMZNPayments/Browse_Node/US_EN/Home/A1_HeroBanner_Desktop_GeneralCategory_CTA.jpg" alt=""/>
-        <div className="checkout__title">
-          <h2>Your Shopping Basket</h2>
-          {/* basket Items */}
-          {/* basket Items */}
-          {/* basket Items */}
-          {/* basket Items */}
+        <div>
+        <h2 className="checkout__title">Your Shopping Basket</h2>
+
+        {basket.map(item => (
+          <CheckoutProduct
+            id={item.id}
+            title={item.title}
+            image={item.image}
+            price={item.price}
+            rating={item.rating}
+          />
+        ))}
+        {/* checkout product*/}
+        {/* checkout product*/}
+        {/* checkout product*/}
+        {/* checkout product*/}
+        {/* checkout product*/}
         </div>
       </div>
 
